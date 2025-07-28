280bded5e6bc7c7b4d141631e7eadc3b6bde7987
snipershady <snipershady@gmail.com>
2025-07-28 15:00:38 +0100

    Basic Test for all parameters of sample json

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -54,10 +54,10 @@ class BasicTest extends AbstractTestCase {
         }
 
         try {
-            $map = ValidationV4Mapper::fromArray($arrayContent);
+            $response = ValidationV4Mapper::fromArray($arrayContent);
             // Puoi fare asserzioni qui, ad esempio:
-            $this->assertNotNull($map);
-            $this->assertInstanceOf(ValidationV4Response::class, $map);
+            $this->assertNotNull($response);
+            $this->assertInstanceOf(ValidationV4Response::class, $response);
         } catch (Throwable $e) {
             fwrite(STDERR, "Exception caught: " . $e::class . "\n");
             fwrite(STDERR, $e->getMessage() . "\n");
@@ -66,7 +66,105 @@ class BasicTest extends AbstractTestCase {
             $this->fail("Exception thrown during fromArray(): " . $e->getMessage());
         }
 
-        var_dump($map);
+        $data = $response->getData();
+        $standard = $data->getAddress()->getStandard();
+        $smart = $data->getAddress()->getSmart();
+        $geo = $data->getGeo();
+        $postal = $data->getPostal();
+        $quality = $response->getQuality()->getAddress();
+        $system = $response->getSystem();
+
+        // Standard address assertions
+        $this->assertEquals('ITA', $standard->getIso3());
+        $this->assertEquals('Italia', $standard->getCountry());
+        $this->assertEquals('Veneto', $standard->getRegion());
+        $this->assertEquals('Verona', $standard->getProvince());
+        $this->assertEquals('VR', $standard->getProvinceCode());
+        $this->assertEquals('Verona', $standard->getCity());
+        $this->assertEquals('37135', $standard->getZipcode());
+        $this->assertEquals('Via', $standard->getStreetType());
+        $this->assertEquals('Antonio Pacinotti', $standard->getStreetName());
+        $this->assertEquals('Via Antonio Pacinotti', $standard->getStreet());
+        $this->assertEquals('Via Antonio Pacinotti,4/B', $standard->getAddress());
+        $this->assertEquals('Via Antonio Pacinotti,4/B, Verona, VR, Veneto, 37135, Italia', $standard->getFullAddress());
+        $this->assertEquals('4/B', $standard->getHn());
+        $this->assertEquals('VERONA', $standard->getPostalTown1());
+
+        $admCodes = $standard->getAdmCode();
+        $this->assertCount(12, $admCodes);
+        $this->assertEquals('50137', $admCodes[0]->getType());
+        $this->assertEquals('VR', $admCodes[0]->getValue());
+
+        $this->assertEquals('30030', $admCodes[1]->getType());
+        $this->assertEquals('11700', $admCodes[1]->getValue());
+
+        $this->assertEquals('30000', $admCodes[2]->getType());
+        $this->assertEquals('L781', $admCodes[2]->getValue());
+
+        $this->assertEquals('30021', $admCodes[3]->getType());
+        $this->assertEquals('023091', $admCodes[3]->getValue());
+
+        $this->assertEquals('30060', $admCodes[4]->getType());
+        $this->assertEquals('086', $admCodes[4]->getValue());
+
+        $this->assertEquals('30061', $admCodes[5]->getType());
+        $this->assertEquals('660999', $admCodes[5]->getValue());
+
+        $this->assertEquals('30050', $admCodes[6]->getType());
+        $this->assertEquals('0777000', $admCodes[6]->getValue());
+
+        $this->assertEquals('30071', $admCodes[7]->getType());
+        $this->assertEquals('IT', $admCodes[7]->getValue());
+
+        $this->assertEquals('30070', $admCodes[8]->getType());
+        $this->assertEquals('ITA', $admCodes[8]->getValue());
+
+        $this->assertEquals('30072', $admCodes[9]->getType());
+        $this->assertEquals('380', $admCodes[9]->getValue());
+
+        $this->assertEquals('30114', $admCodes[10]->getType());
+        $this->assertEquals('ITALIA', $admCodes[10]->getValue());
+
+        $this->assertEquals('30022', $admCodes[11]->getType());
+        $this->assertEquals('005', $admCodes[11]->getValue());
+
+        $egonCode = $standard->getEgonCode();
+        $this->assertEquals(38000004730, $egonCode->getCity());
+        $this->assertEquals(38000073526, $egonCode->getStreet());
+        $this->assertEquals(380100008326045, $egonCode->getHn());
+
+        // Smart address assertions
+        $this->assertEquals('Italia', $smart->getCountry());
+        $this->assertEquals('Verona', $smart->getAdministrativeLevel());
+        $this->assertEquals('Verona', $smart->getCity());
+        $this->assertEquals('Via Antonio Pacinotti,4/B', $smart->getAddress());
+        $this->assertEquals('37135', $smart->getZipcode());
+
+        // Geo
+        $this->assertEquals('45.40616800, 10.97379600', $geo->getLatLong());
+        $this->assertEquals('A41-111', $geo->getGeoLevel());
+        $this->assertEquals('0230910000922', $geo->getCensusCode());
+
+        // Postal
+        $this->assertEquals('VIA ANTONIO PACINOTTI 4/B', $postal->getRow4());
+        $this->assertEquals('37135 VERONA VR', $postal->getRow5());
+
+        // Quality
+        $this->assertEquals('0', $quality->getLocality()->getFlag());
+        $this->assertEquals(0, $quality->getLocality()->getCode());
+        $this->assertEquals('Ok', $quality->getLocality()->getDescription());
+
+        $this->assertEquals('1', $quality->getStreet()->getFlag());
+        $this->assertEquals(801, $quality->getStreet()->getCode());
+        $this->assertEquals('Candidato strada con parole in meno', $quality->getStreet()->getDescription());
+
+        $this->assertEquals('0', $quality->getHn()->getFlag());
+        $this->assertEquals(0, $quality->getHn()->getCode());
+        $this->assertEquals('Ok', $quality->getHn()->getDescription());
+
+        // System
+        $this->assertEquals(0, $system->getRetCode());
+        $this->assertEquals('Ok', $system->getDesRetCode());
 
         $this->assertTrue(true);
     }
