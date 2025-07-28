775c332089ad51a1779041d5dfc4dbd8bc22558b
snipershady <snipershady@gmail.com>
2025-07-28 13:30:48 +0100

    Request enum and Servic init

---
+++ src/Enum/CountryCodeAlpha3Enum.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace Egon\Enum;
+
+/*
+ * Copyright (C) 2022 Stefano Perrini <perrini.stefano@gmail.com> aka La Matrigna
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Description of CountryCodeAlpha3Enum
+ *
+ * @author Stefano Perrini <perrini.stefano@gmail.com> aka La Matrigna
+ */
+enum CountryCodeAlpha3Enum: string {
+
+    case AFGHANISTAN = 'AFG';
+    case ALBANIA = 'ALB';
+    case ALGERIA = 'DZA';
+    case AMERICAN_SAMOA = 'ASM';
+    case ANDORRA = 'AND';
+    case ANGOLA = 'AGO';
+    case ANGUILLA = 'AIA';
+    case ANTARCTICA = 'ATA';
+    case ANTIGUA_AND_BARBUDA = 'ATG';
+    case ARGENTINA = 'ARG';
+    case ARMENIA = 'ARM';
+    case ARUBA = 'ABW';
+    case AUSTRALIA = 'AUS';
+    case AUSTRIA = 'AUT';
+    case AZERBAIJAN = 'AZE';
+    case BAHAMAS = 'BHS';
+    case BAHRAIN = 'BHR';
+    case BANGLADESH = 'BGD';
+    case BARBADOS = 'BRB';
+    case BELARUS = 'BLR';
+    case BELGIUM = 'BEL';
+    case BELIZE = 'BLZ';
+    case BENIN = 'BEN';
+    case BERMUDA = 'BMU';
+    case BHUTAN = 'BTN';
+    case BOLIVIA_PLURINATIONAL_STATE_OF = 'BOL';
+    case BONAIRE_SINT_EUSTATIUS_AND_SABA = 'BES';
+    case BOSNIA_AND_HERZEGOVINA = 'BIH';
+    case BOTSWANA = 'BWA';
+    case BOUVET_ISLAND = 'BVT';
+    case BRAZIL = 'BRA';
+    case BRITISH_INDIAN_OCEAN_TERRITORY = 'IOT';
+    case BRUNEI_DARUSSALAM = 'BRN';
+    case BULGARIA = 'BGR';
+    case BURKINA_FASO = 'BFA';
+    case BURUNDI = 'BDI';
+    case CABO_VERDE = 'CPV';
+    case CAMBODIA = 'KHM';
+    case CAMEROON = 'CMR';
+    case CANADA = 'CAN';
+    case CAYMAN_ISLANDS = 'CYM';
+    case CENTRAL_AFRICAN_REPUBLIC = 'CAF';
+    case CHAD = 'TCD';
+    case CHILE = 'CHL';
+    case CHINA = 'CHN';
+    case CHRISTMAS_ISLAND = 'CXR';
+    case COCOS_KEELING_ISLANDS = 'CCK';
+    case COLOMBIA = 'COL';
+    case COMOROS = 'COM';
+    case CONGO = 'COG';
+    case CONGO_THE_DEMOCRATIC_REPUBLIC_OF_THE = 'COD';
+    case COOK_ISLANDS = 'COK';
+    case COSTA_RICA = 'CRI';
+    case CROATIA = 'HRV';
+    case CUBA = 'CUB';
+    case CURAÇAO = 'CUW';
+    case CYPRUS = 'CYP';
+    case CZECHIA = 'CZE';
+    case CÔTE_DIVOIRE = 'CIV';
+    case DENMARK = 'DNK';
+    case DJIBOUTI = 'DJI';
+    case DOMINICA = 'DMA';
+    case DOMINICAN_REPUBLIC = 'DOM';
+    case ECUADOR = 'ECU';
+    case EGYPT = 'EGY';
+    case EL_SALVADOR = 'SLV';
+    case EQUATORIAL_GUINEA = 'GNQ';
+    case ERITREA = 'ERI';
+    case ESTONIA = 'EST';
+    case ESWATINI = 'SWZ';
+    case ETHIOPIA = 'ETH';
+    case FALKLAND_ISLANDS_MALVINAS = 'FLK';
+    case FAROE_ISLANDS = 'FRO';
+    case FIJI = 'FJI';
+    case FINLAND = 'FIN';
+    case FRANCE = 'FRA';
+    case FRENCH_GUIANA = 'GUF';
+    case FRENCH_POLYNESIA = 'PYF';
+    case FRENCH_SOUTHERN_TERRITORIES = 'ATF';
+    case GABON = 'GAB';
+    case GAMBIA = 'GMB';
+    case GEORGIA = 'GEO';
+    case GERMANY = 'DEU';
+    case GHANA = 'GHA';
+    case GIBRALTAR = 'GIB';
+    case GREECE = 'GRC';
+    case GREENLAND = 'GRL';
+    case GRENADA = 'GRD';
+    case GUADELOUPE = 'GLP';
+    case GUAM = 'GUM';
+    case GUATEMALA = 'GTM';
+    case GUERNSEY = 'GGY';
+    case GUINEA = 'GIN';
+    case GUINEA_BISSAU = 'GNB';
+    case GUYANA = 'GUY';
+    case HAITI = 'HTI';
+    case HEARD_ISLAND_AND_MCDONALD_ISLANDS = 'HMD';
+    case HOLY_SEE_VATICAN_CITY_STATE = 'VAT';
+    case HONDURAS = 'HND';
+    case HONG_KONG = 'HKG';
+    case HUNGARY = 'HUN';
+    case ICELAND = 'ISL';
+    case INDIA = 'IND';
+    case INDONESIA = 'IDN';
+    case IRAN_ISLAMIC_REPUBLIC_OF = 'IRN';
+    case IRAQ = 'IRQ';
+    case IRELAND = 'IRL';
+    case ISLE_OF_MAN = 'IMN';
+    case ISRAEL = 'ISR';
+    case ITALY = 'ITA';
+    case JAMAICA = 'JAM';
+    case JAPAN = 'JPN';
+    case JERSEY = 'JEY';
+    case JORDAN = 'JOR';
+    case KAZAKHSTAN = 'KAZ';
+    case KENYA = 'KEN';
+    case KIRIBATI = 'KIR';
+    case KOREA_DEMOCRATIC_PEOPLES_REPUBLIC_OF = 'PRK';
+    case KOREA_REPUBLIC_OF = 'KOR';
+    case KUWAIT = 'KWT';
+    case KYRGYZSTAN = 'KGZ';
+    case LAO_PEOPLES_DEMOCRATIC_REPUBLIC = 'LAO';
+    case LATVIA = 'LVA';
+    case LEBANON = 'LBN';
+    case LESOTHO = 'LSO';
+    case LIBERIA = 'LBR';
+    case LIBYA = 'LBY';
+    case LIECHTENSTEIN = 'LIE';
+    case LITHUANIA = 'LTU';
+    case LUXEMBOURG = 'LUX';
+    case MACAO = 'MAC';
+    case MADAGASCAR = 'MDG';
+    case MALAWI = 'MWI';
+    case MALAYSIA = 'MYS';
+    case MALDIVES = 'MDV';
+    case MALI = 'MLI';
+    case MALTA = 'MLT';
+    case MARSHALL_ISLANDS = 'MHL';
+    case MARTINIQUE = 'MTQ';
+    case MAURITANIA = 'MRT';
+    case MAURITIUS = 'MUS';
+    case MAYOTTE = 'MYT';
+    case MEXICO = 'MEX';
+    case MICRONESIA_FEDERATED_STATES_OF = 'FSM';
+    case MOLDOVA_REPUBLIC_OF = 'MDA';
+    case MONACO = 'MCO';
+    case MONGOLIA = 'MNG';
+    case MONTENEGRO = 'MNE';
+    case MONTSERRAT = 'MSR';
+    case MOROCCO = 'MAR';
+    case MOZAMBIQUE = 'MOZ';
+    case MYANMAR = 'MMR';
+    case NAMIBIA = 'NAM';
+    case NAURU = 'NRU';
+    case NEPAL = 'NPL';
+    case NETHERLANDS = 'NLD';
+    case NEW_CALEDONIA = 'NCL';
+    case NEW_ZEALAND = 'NZL';
+    case NICARAGUA = 'NIC';
+    case NIGER = 'NER';
+    case NIGERIA = 'NGA';
+    case NIUE = 'NIU';
+    case NORFOLK_ISLAND = 'NFK';
+    case NORTH_MACEDONIA = 'MKD';
+    case NORTHERN_MARIANA_ISLANDS = 'MNP';
+    case NORWAY = 'NOR';
+    case OMAN = 'OMN';
+    case PAKISTAN = 'PAK';
+    case PALAU = 'PLW';
+    case PALESTINE_STATE_OF = 'PSE';
+    case PANAMA = 'PAN';
+    case PAPUA_NEW_GUINEA = 'PNG';
+    case PARAGUAY = 'PRY';
+    case PERU = 'PER';
+    case PHILIPPINES = 'PHL';
+    case PITCAIRN = 'PCN';
+    case POLAND = 'POL';
+    case PORTUGAL = 'PRT';
+    case PUERTO_RICO = 'PRI';
+    case QATAR = 'QAT';
+    case ROMANIA = 'ROU';
+    case RUSSIAN_FEDERATION = 'RUS';
+    case RWANDA = 'RWA';
+    case RÉUNION = 'REU';
+    case SAINT_BARTHÉLEMY = 'BLM';
+    case SAINT_HELENA_ASCENSION_AND_TRISTAN_DA_CUNHA = 'SHN';
+    case SAINT_KITTS_AND_NEVIS = 'KNA';
+    case SAINT_LUCIA = 'LCA';
+    case SAINT_MARTIN_FRENCH_PART = 'MAF';
+    case SAINT_PIERRE_AND_MIQUELON = 'SPM';
+    case SAINT_VINCENT_AND_THE_GRENADINES = 'VCT';
+    case SAMOA = 'WSM';
+    case SAN_MARINO = 'SMR';
+    case SAO_TOME_AND_PRINCIPE = 'STP';
+    case SAUDI_ARABIA = 'SAU';
+    case SENEGAL = 'SEN';
+    case SERBIA = 'SRB';
+    case SEYCHELLES = 'SYC';
+    case SIERRA_LEONE = 'SLE';
+    case SINGAPORE = 'SGP';
+    case SINT_MAARTEN_DUTCH_PART = 'SXM';
+    case SLOVAKIA = 'SVK';
+    case SLOVENIA = 'SVN';
+    case SOLOMON_ISLANDS = 'SLB';
+    case SOMALIA = 'SOM';
+    case SOUTH_AFRICA = 'ZAF';
+    case SOUTH_GEORGIA_AND_THE_SOUTH_SANDWICH_ISLANDS = 'SGS';
+    case SOUTH_SUDAN = 'SSD';
+    case SPAIN = 'ESP';
+    case SRI_LANKA = 'LKA';
+    case SUDAN = 'SDN';
+    case SURINAME = 'SUR';
+    case SVALBARD_AND_JAN_MAYEN = 'SJM';
+    case SWEDEN = 'SWE';
+    case SWITZERLAND = 'CHE';
+    case SYRIAN_ARAB_REPUBLIC = 'SYR';
+    case TAIWAN_PROVINCE_OF_CHINA = 'TWN';
+    case TAJIKISTAN = 'TJK';
+    case TANZANIA_UNITED_REPUBLIC_OF = 'TZA';
+    case THAILAND = 'THA';
+    case TIMOR_LESTE = 'TLS';
+    case TOGO = 'TGO';
+    case TOKELAU = 'TKL';
+    case TONGA = 'TON';
+    case TRINIDAD_AND_TOBAGO = 'TTO';
+    case TUNISIA = 'TUN';
+    case TURKEY = 'TUR';
+    case TURKMENISTAN = 'TKM';
+    case TURKS_AND_CAICOS_ISLANDS = 'TCA';
+    case TUVALU = 'TUV';
+    case UGANDA = 'UGA';
+    case UKRAINE = 'UKR';
+    case UNITED_ARAB_EMIRATES = 'ARE';
+    case UNITED_KINGDOM = 'GBR';
+    case UNITED_STATES = 'USA';
+    case UNITED_STATES_MINOR_OUTLYING_ISLANDS = 'UMI';
+    case URUGUAY = 'URY';
+    case UZBEKISTAN = 'UZB';
+    case VANUATU = 'VUT';
+    case VENEZUELA_BOLIVARIAN_REPUBLIC_OF = 'VEN';
+    case VIET_NAM = 'VNM';
+    case VIRGIN_ISLANDS_BRITISH = 'VGB';
+    case VIRGIN_ISLANDS_U_S = 'VIR';
+    case WALLIS_AND_FUTUNA = 'WLF';
+    case WESTERN_SAHARA = 'ESH';
+    case YEMEN = 'YEM';
+    case ZAMBIA = 'ZMB';
+    case ZIMBABWE = 'ZWE';
+    case ÅLAND_ISLANDS = 'ALA';
+}
